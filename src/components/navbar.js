cbb921846f3e50a25a4a20b7c165d3ac4b945589
Bilal AlSayed Ahmad <bilalalsayed95@gmail.com>
2021-12-09 15:32:25 +0200

    c
    c

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -41,37 +41,3 @@ export default NavBar;
 
 
 
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { Link, NavLink } from 'react-router-dom';
-// import Movies from './movies';
-import Customers from './customers';
-import Rentals from './rentals';
-
-// const NavBar = () => {
-//   return ( 
-//     <nav className="navbar navbar-expand-lg navbar-dark">
-//       <Link className="navbar-brand" to="/">Movies</Link>
-//       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-//       <div className="collapse navbar-collapse" id="navbarNav">
-//         <div className="navbar-nav">
-//           <NavLink to="/movies" className="nav-item nav-link">Movies</NavLink>
-//           <NavLink to="/customers" className="nav-item nav-link">Customers</NavLink>
-//           <NavLink to="/rentals" className="nav-item nav-link">Rentals</NavLink>
-//         </div>
-//       </div>
-//     </nav>
-//    );
-// }
- 
-// export default NavBar;
